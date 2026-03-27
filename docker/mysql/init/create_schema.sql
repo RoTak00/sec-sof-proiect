1093625ebae2fff084659f9d96834879f2b4c7a8
Robert Takacs <RoTak@protonmail.com>
2026-03-27 10:17:58 +0200

    Add database structure

--- docker/mysql/init/create_schema.sql
+++ docker/mysql/init/create_schema.sql
@@ -1,1 +1,37 @@
 USE app;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `password` varchar(255) NOT NULL,
+  `role` enum('user', 'analyst', 'admin') NOT NULL DEFAULT 'user',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `tickets` (
+  `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NULL,
+  `severity` enum ('low', 'medium', 'high') NOT NULL,
+  `status` enum('open', 'in_progress', 'resolved') NOT NULL DEFAULT 'open',
+  `owner_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ticket_id`),
+  FOREIGN KEY (`owner_id`) REFERENCES `users`(`user_id`)
+);
+
+CREATE TABLE audit_logs (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `action` varchar(255) NOT NULL,
+  `resource` varchar(255) NOT NULL,
+  `resource_id` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip_address` varchar(255) NOT NULL,
+  PRIMARY KEY (`log_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
+);
+
+
